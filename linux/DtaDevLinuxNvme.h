8dcb0ee8eb6084b21caf1fdce8a3d13d262af5b1
Bilal Ahmad <bilalahmaducc@gmail.com>
2025-10-31 12:05:57 +0500

    Updated build scripts and configs for Ubuntu 24.04

--- linux/DtaDevLinuxNvme.h
+++ linux/DtaDevLinuxNvme.h
@@ -18,12 +18,9 @@ along with sedutil.  If not, see <http://www.gnu.org/licenses/>.
 
  * C:E********************************************************************** */
 #pragma once
-#include <linux/version.h>
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0)
+// Ubuntu 24.04 uses kernel 6.8, which has nvme_ioctl.h
+// For older kernels (< 4.4.0), nvme.h would be used instead
 #include <linux/nvme_ioctl.h>
-#else
-#include <linux/nvme.h>
-#endif
 #include "DtaStructures.h"
 #include "DtaDevLinuxDrive.h"
 
